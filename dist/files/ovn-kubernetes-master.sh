16e1a3cf0c34be1e2d449b8ff6ddf3f8d01beaab
“gwest“ <gwest@redhat.com>
2018-11-01 07:56:33 -0500

    Add restart loops

--- dist/files/ovn-kubernetes-master.sh
+++ dist/files/ovn-kubernetes-master.sh
@@ -7,11 +7,15 @@ set -o pipefail
 source /etc/sysconfig/ovn-kubernetes
 
 function ovn-kubernetes-master() {
+  while true; do
   echo "Enable and start ovn-kubernetes master services"
   /usr/bin/ovnkube \
 	--cluster-subnet "${cluster_cidr}" \
 	--init-master `hostname` \
 	--net-controller
+  echo "Master Failed - Sleeping"
+  sleep 5
+  done
 }
 
 ovn-kubernetes-master
--- dist/files/ovn-kubernetes-node.sh
+++ dist/files/ovn-kubernetes-node.sh
@@ -8,10 +8,14 @@ source /etc/sysconfig/ovn-kubernetes
 
 function ovn-kubernetes-node() {
 
+  while true; do
   echo "Enable and start ovn-kubernetes node services"
   /usr/bin/ovnkube \
 	--cluster-subnet "${cluster_cidr}" \
 	--init-node `hostname`
+ echo "ovn-kubernetes-node failed - sleeping"
+ sleep 5
+ done
 }
 
 ovn-kubernetes-node
